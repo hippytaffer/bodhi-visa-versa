99d3157b2c199fa4f397da9cb2d26041b4f9043a
Bodhi <craig.langman@gmail.com>
2021-03-01 11:40:34 -0800

    Set color to E8B66B (Same as text in clock module)

--- MokshaViceVersa-Icons/filecolor.sh
+++ MokshaViceVersa-Icons/filecolor.sh
@@ -1,4 +1,6 @@
-COLOR="AE9066"
+COLOR="E8B66B"
 if ! mogrify -fill "#""$COLOR" -colorize 100 $1; then
 	echo "edit file first to set color.  usage: filecolor.sh filename"
+else
+	echo Changed "$1" to "$COLOR"...
 fi
--- MokshaViceVersa-Icons/reseticons.sh
+++ MokshaViceVersa-Icons/reseticons.sh
@@ -1,3 +1,4 @@
+echo Removing icon cache, restarting efreet and moksha.
 rm -rf ~/.cache/efreet
 killall efreetd
 enlightenment_remote -restart
