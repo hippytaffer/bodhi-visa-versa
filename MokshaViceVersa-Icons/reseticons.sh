005e824e6b1622f72d2b55a3d1ecf64ced8dae99
Bodhi <craig.langman@gmail.com>
2021-03-02 18:56:56 -0800

    minor icon updates

--- MokshaViceVersa-Icons/reseticons.sh
+++ MokshaViceVersa-Icons/reseticons.sh
@@ -1,7 +1,10 @@
-echo Removing icon cache and restarting efreet.
+printf "\nRemoving icon cache and restarting related daemon (efreetd)...\n"
 rm -rf ~/.cache/efreet
 killall efreetd
-#enlightenment_remote -restart
-echo Wait for moksha restart...
-sleep 5 
+enlightenment_remote -restart &
+echo "Waiting (not done yet), will beep when all done!"
+sleep 10
+echo Restarting Moksha...
 enlightenment_remote -restart
+echo -ne '\007'
+echo Tada!
