0aeb09496ac716dea0fa0387f8db8f7b9c61a825
Bodhi <craig.langman@gmail.com>
2021-04-25 09:00:05 -0700

    Improve steam tray icon (but its still bad)

--- usr/share/icons/MokshaViceVersa-Icons/reseticons.sh
+++ usr/share/icons/MokshaViceVersa-Icons/reseticons.sh
@@ -2,10 +2,10 @@ printf "\nRemoving icon cache and restarting related daemon (efreetd)...\n"
 rm -rf ~/.cache/efreet
 killall efreetd
 enlightenment_remote -restart &
-echo "Waiting (not done yet), chime will sound when all done!"
+echo "Waiting (not done yet), chime will sound when done!"
 sleep 10
 while pgrep -u $USER efreet_icon_cache_create > /dev/null; do sleep 1; done
 echo Restarting Moksha...
 enlightenment_remote -restart
 echo -ne '\007'
-echo Tada!
+echo It may take several more minutes for Moksha to update its cache.
