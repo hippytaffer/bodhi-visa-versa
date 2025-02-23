1ec0eadcea314bc1161d2557348a758bf9bdc76b
Robert Wiley <ylee@bodhilinux.com>
2025-02-23 04:24:28 -0500

    Normalize gtk theme name

--- build-legacy.sh
+++ build-legacy.sh
@@ -4,6 +4,8 @@
 printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
 cd bodhi-vice-versa
 sed -i 's/MokshaViceVersa/MokshaViceVersa-Icons/g' dark_revolution.edc
+sed -i 's/MokshaViceVersa/MokshaViceVersa-GTKs/g' dark_revolution.edc
 ./build.sh
 sed -i 's/MokshaViceVersa-Icons/MokshaViceVersa/g' dark_revolution.edc
+sed -i 's/MokshaViceVersa-GTK/MokshaViceVersa/g' dark_revolution.edc
 cd ..
